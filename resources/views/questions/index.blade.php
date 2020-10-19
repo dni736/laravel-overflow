b24afc7f5e8043bebf729498e666906b61192d21
dni736 <dima.nikolishvili@gmail.com>
2020-10-20 00:56:42 +0400

    Auth using Policies

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -17,7 +17,7 @@
 
                 <div class="card-body">
                     @include('layouts._messages')
-                    @foreach ($questions as $question)
+                    @foreach ($questions ?? '' as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
                                 <div class="vote">
@@ -40,16 +40,16 @@
                                         <a href="{{$question->url}}">{{$question->title}}</a>
                                     </h3>
                                     <div class="ml-auto">
-                                    @if (Auth::check() && Auth::user()->can('update-question', $question))
+                                    @can('update', $question)
                                         <a href="{{route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
-                                    @endif
-                                    @if (Auth::check() && Auth::user()->can('delete-question', $question))
+                                    @endcan
+                                    @can('delete', $question)
                                     <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
                                         @method('DELETE')
                                         @csrf
                                         <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                     </form>
-                                    @endif
+                                    @endcan
                                     </div>
                                 </div>
                                 <p class="lead">
@@ -61,7 +61,7 @@
                         </div>
                         <hr>
                     @endforeach
-                    <div class="mx-auto">{{$questions->links()}}</div>
+                    <div class="mx-auto">{{$questions ?? ''->links()}}</div>
                 </div>
             </div>
         </div>
