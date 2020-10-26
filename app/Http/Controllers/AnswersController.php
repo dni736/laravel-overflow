017bbf0fe4e99e0d69b9040ff5e41f454e48cb5d
dni736 <dima.nikolishvili@gmail.com>
2020-10-27 00:20:34 +0400

    Implement update answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -29,7 +29,7 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
 
@@ -43,7 +43,7 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
 
