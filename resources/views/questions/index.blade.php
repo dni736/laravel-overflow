f52d2934083711bd85d2c92aa2b69c7c8ce317c9
dni736 <dima.nikolishvili@gmail.com>
2020-10-27 01:07:53 +0400

    Update best_answer_id on questions table when an answer removed using…

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -9,59 +9,58 @@
                     <div class="d-flex align-items-center">
                         <h2>All Questions</h2>
                         <div class="ml-auto">
-                        <a href="{{route('questions.create')}}" class="btn btn-outline-secondary">Ask Question</a>
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
                         </div>
                     </div>
                     
                 </div>
 
                 <div class="card-body">
-                    @include('layouts._messages')
+                    @include ('layouts._messages')
+
                     @foreach ($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
                                 <div class="vote">
-                                <strong>{{$question->votes}} </strong>{{Str::plural('vote', $question->votes)}}
-                                </div>
-                            
-                            
-                                <div class="status {{$question->status}}">
-                                <strong>{{$question->answers_count}} </strong>{{Str::plural('answer', $question->answers_count)}}
-                                </div>
-                            
-                            
+                                    <strong>{{ $question->votes }}</strong> {{ Str::Plural('vote', $question->votes) }}
+                                </div>                            
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers_count }}</strong> {{ Str::Plural('answer', $question->answers_count) }}
+                                </div>                            
                                 <div class="view">
-                                    {{$question->views . " " . Str::plural('view', $question->views)}}
-                                </div>
+                                    {{ $question->views . " " . Str::Plural('view', $question->views) }}
+                                </div>                            
                             </div>
                             <div class="media-body">
                                 <div class="d-flex align-items-center">
-                                    <h3 class="mt-0">
-                                        <a href="{{$question->url}}">{{$question->title}}</a>
-                                    </h3>
+                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                    @if (Auth::check() && Auth::user()->can('update-question', $question))
-                                        <a href="{{route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
-                                    @endif
-                                    @if (Auth::check() && Auth::user()->can('delete-question', $question))
-                                    <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                    </form>
-                                    @endif
+                                        @can ('update', $question)
+                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endcan
+                                        @can ('delete', $question)
+                                            <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                            </form>
+                                        @endcan
                                     </div>
                                 </div>
                                 <p class="lead">
-                                Asked by <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                                <small class="text-muted">{{$question->created_date}}</small>
+                                    Asked by 
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a> 
+                                    <small class="text-muted">{{ $question->created_date }}</small>
                                 </p>
-                                {{Str::limit($question->body,250)}}
-                            </div>
+                                {{ str_limit($question->body, 250) }}
+                            </div>                        
                         </div>
                         <hr>
                     @endforeach
-                    <div class="mx-auto">{{$questions->links()}}</div>
+
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
+                    </div>
                 </div>
             </div>
         </div>
