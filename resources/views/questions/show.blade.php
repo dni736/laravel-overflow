c6f432dff61c8216524748167b2272dd96bf999a
dni736 <dima.nikolishvili@gmail.com>
2020-10-28 18:36:57 +0400

    Implement many to many relationship and make the favorite button work

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -12,12 +12,13 @@
                             <div class="ml-auto">
                                 <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                             </div>
-                        </div>
-    
+                        </div>                        
                     </div>
+
                     <hr>
+
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls">
+                        <div class="d-fex flex-column vote-controls">
                             <a title="This question is useful" class="vote-up">
                                 <i class="fas fa-caret-up fa-3x"></i>
                             </a>
@@ -25,34 +26,43 @@
                             <a title="This question is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Click to mark as fav question (Click again to undo)" class="favorite mt-2 favorited">
+                            <a title="Click to mark as favorite question (Click again to undo)" 
+                                class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
+                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
+                                >
                                 <i class="fas fa-star fa-2x"></i>
-                                <span class="favorites-count">123</span>
+                                <span class="favorites-count">{{ $question->favorites_count }}</span>
                             </a>
+                            <form id="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="POST" style="display:none;">
+                                @csrf
+                                @if ($question->is_favorited)
+                                    @method ('DELETE')
+                                @endif
+                            </form>
                         </div>
-                       <div class="media-body">
-                        {!! $question->body_html !!}
-                        <div class="float-right">
-                         <span class="text-muted">Answered {{$question->created_date}}</span>
-                             <div class="media mt-2">
-                                 <a href="{{$question->user->url}}" class="pr-2">
-                                     <img src="{{$question->user->avatar}}">
-                                 </a>
-                                 <div class="media-body mt-1">
-                                     <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                                 </div>
-                             </div>
-                         </div>
-                       </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Answered {{ $question->created_date }}</span>
+                                <div class="media mt-2">
+                                    <a href="{{ $question->user->url }}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    @include('answers._index', [
+    @include ('answers._index', [
         'answers' => $question->answers,
-        'answersCount' => $question->answers_count
+        'answersCount' => $question->answers_count,
     ])
-    @include('answers._create')
+    @include ('answers._create')
 </div>
 @endsection
