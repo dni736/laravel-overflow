3a79e87f6520638b07de24c6ac88c9f88de263a7
dni736 <dima.nikolishvili@gmail.com>
2020-11-06 22:51:45 +0400

    Create a basic VueJS component

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -26,10 +26,9 @@
                             <div class="row">
                                 <div class="col-4"></div>
                                 <div class="col-4"></div>
-                                <div class="col-4">@include('shared._author', [
-                                    'model' => $question,
-                                    'label' => 'Asked'
-                                ])</div>
+                                <div class="col-4">
+                                    <user-info :model="{{$question}}" label="Asked"></user-info>
+                                </div>
                             </div>
                         </div>
                     </div>
