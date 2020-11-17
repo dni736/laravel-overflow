37ba00c06f3e2390367c90bfce4e59e46cc553d7
dni736 <dima.nikolishvili@gmail.com>
2020-11-17 12:09:02 +0400

    Ajaxify the Delete Button

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -21,7 +21,7 @@ export default {
             this.editing = false;
         },
         update(){
-            axios.patch(`/questions/${this.questionId}/answers/${this.id}`, {
+            axios.patch(this.endpoint, {
                 body: this.body
             }).then(res => {
                 this.editing = false;
@@ -29,12 +29,25 @@ export default {
             }).catch(err => {
                 alert(err.response.data.message);
             });
+        },
+        destroy(){
+            if (confirm('Are you sure?')){
+                axios.delete(this.endpoint)
+                .then(res=>{
+                    $(this.$el).fadeOut(500, () =>{
+                        alert(res.data.message);
+                    })
+                });
+            }
         }
     },
 
     computed: {
         isInvalid(){
             return this.body.length < 10;
+        },
+        endpoint(){
+            return `/questions/${this.questionId}/answers/${this.id}`;
         }
     }
 }
