a4e6e559b8992eb2206400131d0b1a32b322ef9d
dni736 <dima.nikolishvili@gmail.com>
2020-11-18 03:36:34 +0400

    Utilizing vue.js mixins

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -14,9 +14,9 @@
                 <div v-html="bodyHtml"></div>
                 <div class="row">
                     <div class="col-4">
-                        <div class="ml-auto">
-                            <a v-if="authorize('modify', answer)" @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>
-                            <button v-if="authorize('modify', answer)" @click="destroy" class="btn btn-sm btn-outline-danger">Delete</button>                                
+                        <div class="ml-auto">                            
+                            <a v-if="authorize('modify', answer)" @click.prevent="edit" class="btn btn-sm btn-outline-info">Edit</a>                                                        
+                            <button v-if="authorize('modify', answer)" @click="destroy" class="btn btn-sm btn-outline-danger">Delete</button>                                                            
                         </div>
                     </div>
                     <div class="col-4"></div>
@@ -28,15 +28,21 @@
         </div>
     </div>
 </template>
+
 <script>
 import Vote from './Vote.vue';
 import UserInfo from './UserInfo.vue';
+import modification from '../mixins/modification'
+
 export default {
     props: ['answer'],
+
+    mixins: [modification],
+
     components: { Vote, UserInfo },
-    data(){
-        return{
-            editing: false,
+
+    data () {
+        return {            
             body: this.answer.body,
             bodyHtml: this.answer.body_html,
             id: this.answer.id,
@@ -44,59 +50,37 @@ export default {
             beforeEditCache: null
         }
     },
+
     methods: {
-        edit(){
-            this.beforeEditCache = this.body;
-            this.editing = true;
+        setEditCache () {
+            this.beforeEditCache = this.body;                        
         },
-        cancel(){
-            this.body = this.beforeEditCache;
-            this.editing = false;
+
+        restoreFromCache () {
+            this.body = this.beforeEditCache;            
         },
-        update(){
-            axios.patch(this.endpoint, {
+
+        payload () {
+            return {
                 body: this.body
-            }).then(res => {
-                this.editing = false;
-                this.bodyHtml = res.data.body_html;
-                this.$toast.success(res.data.message, "Success!", {timeout:3000});
-            }).catch(err => {
-                this.$toast.error(response.data.message, "Error!", {timeout:3000});
-            });
+            };
         },
-        destroy(){
-            this.$toast.question('Are you sure about that?', "Confirm", {
-            timeout: 20000,
-            close: false,
-            overlay: true,
-            displayMode: 'once',
-            id: 'question',
-            zindex: 999,
-            title: 'Hey',
-            position: 'center',
-            buttons: [
-                ['<button><b>YES</b></button>',  (instance, toast) => {
-                    axios.delete(this.endpoint)
-                    .then(res=>{
-                        this.$emit('deleted')
-                    });
-                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-                }, true],
-                ['<button>NO</button>', function (instance, toast) {
-        
-                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-        
-                }],
-            ]
-        });
-        }
+
+        delete () {
+            axios.delete(this.endpoint)
+                .then(res => {
+                    this.$toast.success(res.data.message, "Success", { timeout: 2000 });
+                    this.$emit('deleted')
+                });
+        } 
     },
 
     computed: {
-        isInvalid(){
+        isInvalid () {
             return this.body.length < 10;
         },
-        endpoint(){
+
+        endpoint () {
             return `/questions/${this.questionId}/answers/${this.id}`;
         }
     }
--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -33,7 +33,7 @@
 
                     <div class="media">
                         <vote :model="question" name="question"></vote>
-
+                        
                         <div class="media-body">
                             <div v-html="bodyHtml"></div>
                             <div class="row">
@@ -59,81 +59,64 @@
 <script>
 import Vote from './Vote.vue';
 import UserInfo from './UserInfo.vue';
+import modification from '../mixins/modification';
+
 export default {
     props: ['question'],
+
+    mixins: [modification],
+
     components: { Vote, UserInfo },
+
     data () {
         return {
             title: this.question.title,
             body: this.question.body,
-            bodyHtml: this.question.body_html,
-            editing: false,
+            bodyHtml: this.question.body_html,            
             id: this.question.id,
             beforeEditCache: {}
         }
     },
+
     computed: {
         isInvalid () {
             return this.body.length < 10 || this.title.length < 10;
         },
+
         endpoint () {
             return `/questions/${this.id}`;
         }
     },
+
     methods: {
-        edit () {
+        setEditCache () {
             this.beforeEditCache = {
                 body: this.body,
                 title: this.title
-            };
-            this.editing = true;
+            };            
         },
-        cancel () {
+
+        restoreFromCache () {
             this.body = this.beforeEditCache.body;
-            this.title = this.beforeEditCache.title;
-            this.editing = false;
+            this.title = this.beforeEditCache.title;            
         },
-        update () {
-            axios.put(this.endpoint, {
+
+        payload () {
+            return {
                 body: this.body,
                 title: this.title
-            })
-            .catch(({response}) => {
-                this.$toast.error(response.data.message, "Error", { timeout: 3000 });
-            })
-            .then(({data}) => {
-                this.bodyHtml = data.body_html;
-                this.$toast.success(data.message, "Success", { timeout: 3000 });
-                this.editing = false;
-            })
+            };
         },
-        destroy () {
-            this.$toast.question('Are you sure about that?', "Confirm", {
-            timeout: 20000,
-            close: false,
-            overlay: true,
-            displayMode: 'once',
-            id: 'question',
-            zindex: 999,
-            title: 'Hey',            
-            position: 'center',
-            buttons: [
-                ['<button><b>YES</b></button>', (instance, toast) => {
-                    
-                    axios.delete(this.endpoint)
-                    .then(({data}) => {
-                        this.$toast.success(data.message, "Success", { timeout: 2000 });
-                    });
-                    setTimeout(() => {
-                        window.location.href = "/questions";
-                    }, 3000);
-                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-                }, true],
-                ['<button>NO</button>', function (instance, toast) {
-                    instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
-                }],
-            ]            
-            });            
+
+        delete () {
+            axios.delete(this.endpoint)
+                .then(({data}) => {
+                    this.$toast.success(data.message, "Success", { timeout: 2000 });
+                });
+
+                setTimeout(() => {
+                    window.location.href = "/questions";
+                }, 3000);
         }
     }
 }
