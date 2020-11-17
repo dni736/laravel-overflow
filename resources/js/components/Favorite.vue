dc10921624cc5754d5b7c6208ae78aa40667b9b8
dni736 <dima.nikolishvili@gmail.com>
2020-11-17 19:10:28 +0400

    Rewrite the authorization policy into JS code

--- resources/js/components/Favorite.vue
+++ resources/js/components/Favorite.vue
@@ -22,9 +22,6 @@ export default {
         },
         endpoint(){
             return `/questions/${this.id}/favorites`;
-        },
-        signedIn(){
-            return window.Auth.signedIn;
         }
     },
     methods:{
