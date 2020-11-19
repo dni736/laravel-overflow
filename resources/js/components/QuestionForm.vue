338b001d51467632a78cea0b60e483f72718a390
dni736 <dima.nikolishvili@gmail.com>
2020-11-19 22:57:25 +0400

    Create Edit Question component

--- resources/js/components/QuestionForm.vue
+++ resources/js/components/QuestionForm.vue
@@ -25,6 +25,12 @@
 <script>
 import EventBus from '../event-bus'
 export default {
+    props: {
+        isEdit: {
+            type: Boolean,
+            default: false
+        }
+    },
     data () {
         return {
             title: '',
@@ -37,13 +43,27 @@ export default {
     },
     mounted () {
         EventBus.$on('error', errors => this.errors = errors)
+
+        if (this.isEdit) {
+            this.fetchQuestion();
+        }
     },
     computed: {
         buttonText () {
-            return 'Ask Question'
+            return this.isEdit ? 'Update Question' : 'Ask Question'
         }
     },
     methods: {
+        fetchQuestion () {
+            axios.get(`/questions/${this.$route.params.id}`)
+            .then(({ data }) => {
+                this.title = data.title
+                this.body = data.body
+                })
+            .catch(error => {
+                console.log(error.response);
+            })
+        },
         handleSubmit () {
             this.$emit('submitted', {
                 title: this.title,
